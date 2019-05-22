dfb1dfe65a31b5b1af24073d3a626aa31ea111bb
Carolina1992Assen <34693913+Carolina1992Assen@users.noreply.github.com>
2019-05-23 01:43:37 +0200

    Update simple_form.php

--- assignment_3/simple_form.php
+++ assignment_3/simple_form.php
@@ -78,54 +78,7 @@ include __DIR__ . '/tpl/body_start.php';
         </div>
 
 
-   <!-- <div id="showname" style="display:none">
-
-    </div>
-
-    <div id="amsterdam" style="display:none">
-        <p>You're from the capital of the Netherlands!</p>
-    </div>
-
-    <div id="showplace" style="display:none">
-
-
-    </div>
-
-    <form action="simple_form.php" method="GET">
-        <div class="container">
-            <div class="row wp-row">
-                <div class="col-md-12">
-
-
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name">
-                    <div class="valid-feedback">
-                        Looks good!
-                    </div>
-                    <div class="invalid-feedback">
-                        Enter a valid name
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="place">Place</label>
-                    <input type="text" class="form-control" id="place" name="place">
-                    <div class="valid-feedback">
-                        Looks good!
-                    </div>
-                    <div class="invalid-feedback">
-                        Enter a valid place
-                    </div>
-                </div>
-                <button id="submit" type="submit" class="btn btn-primary">Submit</button>
-
-        </div>
-    </div>
-</div>
-    </form> -->
-
-
+   
 <?php
 include __DIR__ . '/tpl/body_end.php';
 ?>
